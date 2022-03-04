d3b6664d381b7d63a7fbb65f51c4e926c9918681
Joseph Garner <joseph.peter.garner@gmail.com>
2022-03-04 17:22:44 +1100

    KP-43: BugFix Mongo Data Lost Docker Upate

--- deploy/Docker.sh
+++ deploy/Docker.sh
@@ -2,4 +2,4 @@ mvn clean package spring-boot:repackage
 docker container stop PlantIrrigationService
 docker image rm plant-irrigation-service
 docker build -t plant-irrigation-service ../
-docker run -d -name PlantIrrigationService -p 8080:8080 plant-irrigation-service
+docker run -d --name PlantIrrigationService -p 8080:8080 plant-irrigation-service
