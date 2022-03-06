f8290061ebcf4c8e8745896faea2261d18beed38
Joseph Garner <joseph.peter.garner@gmail.com>
2022-03-06 19:52:45 +1100

    KP-42: updated Pi4J config

--- src/main/kotlin/com/khnumpottr/plantirrigationservice/controller/PumpController.kt
+++ src/main/kotlin/com/khnumpottr/plantirrigationservice/controller/PumpController.kt
@@ -1,31 +1,37 @@
 package com.khnumpottr.plantirrigationservice.controller
 
-import com.pi4j.io.gpio.*
-import com.pi4j.io.gpio.RaspiPin.GPIO_07
+import com.pi4j.Pi4J
+import com.pi4j.io.gpio.digital.DigitalOutput
+import com.pi4j.io.gpio.digital.DigitalState
 import mu.KotlinLogging
 
 
 class PumpController {
 
-    val gpio = GpioFactory.getInstance()
-    val pump: GpioPinDigitalOutput  = gpio.provisionDigitalOutputPin(
-        GPIO_07,  // PIN NUMBER
-        "waterPump",  // PIN FRIENDLY NAME (optional)
-        PinState.LOW
-    )
+    private val PIN_PUMP = 4
 
-    private var pumpIsActive: Boolean = false;
+    val pi4j = Pi4J.newAutoContext()
+    val pumpConfig = DigitalOutput.newConfigBuilder(pi4j)
+        .id("pump")
+        .name("waterPump")
+        .address(PIN_PUMP)
+        .shutdown(DigitalState.LOW)
+        .initial(DigitalState.LOW)
+        .provider("pigpio-digital-output")
 
-    fun powerPump(moistureLevel:Int){
-        if(moistureLevel < 15 && !pumpIsActive){
+    val pump = pi4j.create(pumpConfig)
+    
+    private var pumpIsActive: Boolean = false
+
+    fun powerPump(moistureLevel: Int) {
+        if (moistureLevel < 15 && !pumpIsActive) {
             LOG.info { "Powering ON Pump" }
             pumpIsActive = true
-            pump.high();
-        }
-        else if(moistureLevel > 60 && pumpIsActive){
+            pump.high()
+        } else if (moistureLevel > 60 && pumpIsActive) {
             LOG.info { "Powering OFF Pump" }
             pumpIsActive = false
-            pump.low();
+            pump.low()
         }
     }
 
